f1bd21f53d52f8da11d5a2c4b36908cd14173a41
Duygu <128137867+duyguortac@users.noreply.github.com>
2023-06-21 03:34:16 +0300

    corrected provider

---
+++ vpc/output.tf
@@ -0,0 +1,14 @@
+output private_subnets {
+    value = module.vpc.
+
+
+
+
+
+
+
+
+    output public_subnets {
+        value = module.vpc.
+    }
+}
--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,3 +1,4 @@
 provider "aws" {
-    region = "var.region"
+  name = "aws"
+  region = "var.region"
 }
